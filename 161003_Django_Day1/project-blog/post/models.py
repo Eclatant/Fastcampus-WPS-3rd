9ef144c0cc27fea1cb806e84f40e786ef24be6f0
Lee HanYeong <dev@azelf.com>
2016-10-04 15:05:11 +0900

    WPS auto upload

--- 161003_Django_Day1/project-blog/post/models.py
+++ 161003_Django_Day1/project-blog/post/models.py
@@ -9,14 +9,19 @@ from django.db import models
 class Post(models.Model):
     # 글 제목
     title = models.CharField(max_length=40)
+
     # 간단설명
     description = models.CharField(max_length=100)
+
     # 본문내용
     content = models.TextField()
+
     # 좋아요 수
     like_count = models.IntegerField(default=0)
+
     # 작성자의 IP주소
     ip_address = models.IPAddressField(blank=True)
+    
     # 생성일자
     created = models.DateTimeField(auto_now_add=True)
 
