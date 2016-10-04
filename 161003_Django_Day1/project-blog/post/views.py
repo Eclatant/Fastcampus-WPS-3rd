9c0acfd192821aea4b22f25a9f85dce6382af58f
Lee HanYeong <dev@azelf.com>
2016-10-04 16:15:42 +0900

    WPS auto upload

--- 161003_Django_Day1/project-blog/post/views.py
+++ 161003_Django_Day1/project-blog/post/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, render_to_response
 from .models import Post
 
 
+# post_list라는 이름의 view
+# Post인스턴스를 전부 가져와 posts라는 이름으로 'post_list.html'템플릿에 전달해준다
 def post_list(request):
     posts = Post.objects.all()
     ret = {
