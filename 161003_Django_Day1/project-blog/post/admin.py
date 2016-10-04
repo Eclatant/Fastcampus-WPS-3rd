e1609b3a1f4ea8e570cb67cb93d6ee43a10ede45
Lee HanYeong <dev@azelf.com>
2016-10-04 15:24:04 +0900

    WPS auto upload

--- 161003_Django_Day1/project-blog/post/admin.py
+++ 161003_Django_Day1/project-blog/post/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import Post
 
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'created', )
+admin.site.register(Post)
 
-admin.site.register(Post, PostAdmin)
+
+
+
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'description', 'created', )
+
+# admin.site.register(Post, PostAdmin)
