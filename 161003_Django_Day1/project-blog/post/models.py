348548ba78374dd14f899f50e7269cb9215cf1f0
Lee HanYeong <dev@azelf.com>
2016-10-04 15:02:21 +0900

    WPS auto upload

--- 161003_Django_Day1/project-blog/post/models.py
+++ 161003_Django_Day1/project-blog/post/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 
+# Post 클래스
+# 데이터베이스에서 테이블이 됩니다
 class Post(models.Model):
     title = models.CharField(max_length=40)
     description = models.CharField(max_length=100)
