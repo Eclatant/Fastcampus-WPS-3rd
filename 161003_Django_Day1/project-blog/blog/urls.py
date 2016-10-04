26779a97f0c6970f63a9009fd608c111faf33607
Lee HanYeong <dev@azelf.com>
2016-10-04 17:01:40 +0900

    WPS auto upload

--- 161003_Django_Day1/project-blog/blog/urls.py
+++ 161003_Django_Day1/project-blog/blog/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+
 # post 애플리케이션의 views모델에서 post_list함수를 가져옵니다
 from post.views import post_list
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # post/list/로 시작하는 주소에는 post_list뷰를 이용해 response를 돌려준다
     url(r'^post/list/', post_list),
 ]
