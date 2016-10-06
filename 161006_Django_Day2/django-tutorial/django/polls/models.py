bc7c4611c4f906174634ee40faccca2f47332ebc
Lee HanYeong <dev@azelf.com>
2016-10-06 11:23:38 +0900

    WPS auto upload

--- 161006_Django_Day2/django-tutorial/django/polls/models.py
+++ 161006_Django_Day2/django-tutorial/django/polls/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField('질문 내용', max_length=200)
     pub_date = models.DateTimeField('date published')
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField('선택한 내용', max_length=200)
     votes = models.IntegerField(default=0)
