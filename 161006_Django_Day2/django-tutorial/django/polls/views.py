bf6b9222d60857193e7789b9c33016f803a62ec6
Lee HanYeong <dev@azelf.com>
2016-10-06 17:39:56 +0900

    WPS auto upload

--- 161006_Django_Day2/django-tutorial/django/polls/views.py
+++ 161006_Django_Day2/django-tutorial/django/polls/views.py
@@ -55,7 +55,9 @@ def results(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        choice_id = request.POST['choice']
+        print('choice_id : %s' % choice_id)
+        selected_choice = question.choice_set.get(pk=choice_id)
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'detail.html', {
             'question': question,
