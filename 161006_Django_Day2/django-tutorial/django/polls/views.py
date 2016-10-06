cf2a58b8001736fa20951620b77e175bfc9299d3
Lee HanYeong <dev@azelf.com>
2016-10-06 15:12:02 +0900

    WPS auto upload

--- 161006_Django_Day2/django-tutorial/django/polls/views.py
+++ 161006_Django_Day2/django-tutorial/django/polls/views.py
@@ -5,15 +5,16 @@ from .models import Question
 
 
 def index(request):
+    # 가장 최근의 Question 5개를 가져온다
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    # 장고 템플릿 폴더에서 index.html파일을 가져온다
+    template = loader.get_template('index.html')
+    # 템플릿을 렌더링 할 때 사용할 변수 dictionary
     context = {
         'latest_question_list': latest_question_list
     }
+    # Http형식으로 응답을 돌려준다. 내용은 template을 context와 request를 사용해서 렌더링한 결과
     return HttpResponse(template.render(context, request))
-    # output = '<br> '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    # return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
