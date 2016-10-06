710d525e650679d8a264845d4cab5bac084b4005
Lee HanYeong <dev@azelf.com>
2016-10-06 17:23:44 +0900

    WPS auto upload

--- 161006_Django_Day2/django-tutorial/django/polls/views.py
+++ 161006_Django_Day2/django-tutorial/django/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
+from django.urls import reverse
 from .models import Question, Choice
 
 
@@ -56,9 +57,14 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-
-
-    return HttpResponse("You're voting on question %s." % question_id)
+        return render(request, 'detail.html', {
+            'question': question,
+            'error_message': 'You didn\`t select a choice',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 # def test_text(request, text=''):
 #     question = get_object_or_404(Question, pk=question_id)
