8ab4b2b4f3aab6ef865a3d419a15ca485a016bd4
Lee HanYeong <dev@azelf.com>
2016-10-06 16:26:17 +0900

    WPS auto upload

--- 161006_Django_Day2/django-tutorial/django/polls/urls.py
+++ 161006_Django_Day2/django-tutorial/django/polls/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 
     # /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 
     # /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
