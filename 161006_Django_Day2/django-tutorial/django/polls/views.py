955b76ef822de52ba19b1f5582af8ecd79810811
Lee HanYeong <dev@azelf.com>
2016-10-06 21:56:39 +0900

    WPS auto upload

--- 161006_Django_Day2/django-tutorial/django/polls/views.py
+++ 161006_Django_Day2/django-tutorial/django/polls/views.py
@@ -28,7 +28,8 @@ def index_backup(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'detail.html', {'question': question})
+    # return render(request, 'detail.html', {'question': question})
+    return render(request, 'detail.html', question)
 
 
 def detail_backup(request, question_id):
