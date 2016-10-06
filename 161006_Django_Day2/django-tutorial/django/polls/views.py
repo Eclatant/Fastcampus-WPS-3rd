07357930a825e3da0ebd638c30afb746d081970e
Lee HanYeong <dev@azelf.com>
2016-10-06 14:58:28 +0900

    WPS auto upload

--- 161006_Django_Day2/django-tutorial/django/polls/views.py
+++ 161006_Django_Day2/django-tutorial/django/polls/views.py
@@ -5,7 +5,7 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
+    output = '<strong>ABC</strong> '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
     # return HttpResponse("Hello, world. You're at the polls index.")
 
