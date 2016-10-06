d543470d5f555c51a308a44c5dd3dbc68e9a6797
Lee HanYeong <dev@azelf.com>
2016-10-06 17:53:53 +0900

    WPS auto upload

--- 161006_Django_Day2/django-tutorial/django/polls/admin.py
+++ 161006_Django_Day2/django-tutorial/django/polls/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question__question_text', 'choice_text', 'votes', )
+
 admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
