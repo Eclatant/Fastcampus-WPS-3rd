31ffef70a14b5ae199e99dcf9f52a8a50f725d93
Lee HanYeong <dev@azelf.com>
2016-10-07 17:44:25 +0900

    WPS auto upload

--- 161007_Django_Day3/django_model/music/models.py
+++ 161007_Django_Day3/django_model/music/models.py
@@ -32,5 +32,5 @@ class Group(models.Model):
 class Membership(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    date_joined = models.DateField()
-    invite_reason = models.CharField(max_length=64)
+    date_joined = models.DateField(null=True, blank=True)
+    invite_reason = models.CharField(max_length=64, null=True, blank=True)
