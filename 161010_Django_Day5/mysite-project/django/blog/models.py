1a799b42698372e4240a9ac4bc2cbc3418daedc2
Lee HanYeong <dev@azelf.com>
2016-10-10 15:45:10 +0900

    WPS auto upload

--- 161010_Django_Day5/mysite-project/django/blog/models.py
+++ 161010_Django_Day5/mysite-project/django/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
@@ -6,4 +7,11 @@ class Post(models.Model):
     text = models.TextField()
     author = models.ForeignKey('auth.User')
     created_date = models.DateTimeField(auto_now_add=True)
-    published_date = models.DateTimeField()
+    published_date = models.DateTimeField(null=True, blank=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
