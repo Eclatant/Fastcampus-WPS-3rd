f32419bf35e6f4dad16bf76948c9422b0aa8f1c9
Lee HanYeong <dev@azelf.com>
2016-10-10 11:20:43 +0900

    WPS auto upload

--- 161010_Django_Day5/django_model/inheritance/models.py
+++ 161010_Django_Day5/django_model/inheritance/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class Student(CommonInfo):
+    home_group = models.CharField(max_length=5)
+
+
+class Teacher(CommonInfo):
+    year = models.CharField(max_length=10)
