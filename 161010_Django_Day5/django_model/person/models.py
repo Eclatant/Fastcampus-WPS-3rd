55b398eaf3731c40585c690f93df1f40d63be4ea
Lee HanYeong <dev@azelf.com>
2016-10-10 10:38:07 +0900

    WPS auto upload

--- 161010_Django_Day5/django_model/person/models.py
+++ 161010_Django_Day5/django_model/person/models.py
@@ -5,6 +5,10 @@ class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
+    @property
+    def full_name(self):
+        return '%s%s' % (self.last_name, self.first_name)
+
 
 class School(models.Model):
     title = models.CharField(max_length=30)
