d19aa94351f685ebc78eed57a893b98c479edbe8
Lee HanYeong <dev@azelf.com>
2016-10-10 15:44:37 +0900

    WPS auto upload

--- 161010_Django_Day5/mysite-project/django/blog/models.py
+++ 161010_Django_Day5/mysite-project/django/blog/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField()
-    author = models.ForeignKey(User)
+    author = models.ForeignKey('auth.User')
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField()
