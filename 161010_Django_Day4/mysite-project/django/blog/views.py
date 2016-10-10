34b3b8212bbe4f3dc7be457fd46c6e1d40ab2905
Lee HanYeong <dev@azelf.com>
2016-10-10 17:40:10 +0900

    WPS auto upload

--- 161010_Django_Day4/mysite-project/django/blog/views.py
+++ 161010_Django_Day4/mysite-project/django/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.utils import timezone
+from django.db.models import Q
 from .models import Post
 
 # 같은 의미
@@ -7,7 +8,11 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects\
+    # posts = Post.objects\
+    #     # .filter(published_date__lte=timezone.now())\
+    #     .order_by('published_date')
+
+    posts = Post.objects \
         .filter(published_date__lte=timezone.now())\
-        .order_by('published_date')
+    .order_by('published_date')
     return render(request, 'blog/post_list.html', {'post_list': posts, 'title': '타이틀 변수는 title키를 이용해서 접근'})
