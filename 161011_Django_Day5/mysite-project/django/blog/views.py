c8320f2170e63e71dc25f402d6bba52fc4441a07
Lee HanYeong <dev@azelf.com>
2016-10-11 11:21:15 +0900

    WPS auto upload

--- 161011_Django_Day5/mysite-project/django/blog/views.py
+++ 161011_Django_Day5/mysite-project/django/blog/views.py
@@ -19,3 +19,11 @@ def post_list(request):
             Q(published_date=None)
         ).order_by('published_date')
     return render(request, 'blog/post_list.html', {'post_list': posts, 'title': '타이틀 변수는 title키를 이용해서 접근'})
+
+
+def post_detail(request, pk):
+    """
+    post_detail뷰(지금 작업중인 뷰)에서 전달받은 pk를 이용해서
+    pk값(id값)이 전달받은 pk인 Post객체를 Query하여 post라는 변수에 할당
+    해당 변수를 render함수를 이용, post_detail.html템플릿을 이용해 리턴 (post변수는 'post'키로 전달되도록 한다)
+    """
