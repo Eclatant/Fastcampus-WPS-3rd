a5bfb77632294ef242a38cd0084834edb74d113e
Lee HanYeong <dev@azelf.com>
2016-10-11 17:20:52 +0900

    WPS auto upload

--- 161011_Django_Day5/mysite-project/django_app/member/views.py
+++ 161011_Django_Day5/mysite-project/django_app/member/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-
+from django.contrib.auth import \
+    authenticate as auth_authenticate, \
+    login as auth_login
 # https://docs.djangoproject.com/en/1.10/
 # topics/auth/default/#auth-web-requests
 
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth_authenticate(
+        username=username,
+        password=password
+    )
+
+    if user is not None:
+        login(request, user)
+        pass
+    else:
+        pass
