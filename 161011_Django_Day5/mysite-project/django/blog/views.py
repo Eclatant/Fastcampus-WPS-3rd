e931a949b0ee4165cd67b69a85ee6720ef5e45fe
Lee HanYeong <dev@azelf.com>
2016-10-11 11:46:53 +0900

    WPS auto upload

--- 161011_Django_Day5/mysite-project/django/blog/views.py
+++ 161011_Django_Day5/mysite-project/django/blog/views.py
@@ -27,7 +27,12 @@ def post_detail(request, pk):
     pk값(id값)이 전달받은 pk인 Post객체를 Query하여 post라는 변수에 할당
     해당 변수를 render함수를 이용, post_detail.html템플릿을 이용해 리턴 (post변수는 'post'키로 전달되도록 한다)
     """
-    post = Post.objects.get(pk=pk)
+    # 아래와 같이 쓸 경우, pk값에 해당하는 Post객체가 존재하지 않을 경우
+    # DoesNotExist에러 발생
+    # post = Post.objects.get(pk=pk)
+
+    # Post객체가 존재하지 않을 경우에는 404Error를 리턴해준다
+    post = get_object_or_404(Post, pk=pk)
     context = {
         'post': post,
     }
