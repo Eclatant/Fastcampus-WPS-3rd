d95c83ee12e428ddb04bdce86055f08ca5e1340f
Lee HanYeong <dev@azelf.com>
2016-10-11 17:25:50 +0900

    WPS auto upload

--- 161011_Django_Day5/mysite-project/django_app/member/views.py
+++ 161011_Django_Day5/mysite-project/django_app/member/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.contrib.auth import \
     authenticate as auth_authenticate, \
     login as auth_login
 # https://docs.djangoproject.com/en/1.10/
 # topics/auth/default/#auth-web-requests
+
 
 def login(request):
     username = request.POST['username']
@@ -15,6 +17,6 @@ def login(request):
 
     if user is not None:
         auth_login(request, user)
-        pass
+        return HttpResponse('로그인 되었습니다')
     else:
-        pass
+        return HttpResponse('로그인에 실패하였습니다')
