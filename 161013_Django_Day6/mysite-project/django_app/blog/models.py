4f6c3990dfa2d36ee328c08a029b10bcf149f8c4
Lee HanYeong <dev@azelf.com>
2016-10-13 16:53:26 +0900

    WPS auto upload

--- 161013_Django_Day6/mysite-project/django_app/blog/models.py
+++ 161013_Django_Day6/mysite-project/django_app/blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.conf import settings
+
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=50)
     text = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
