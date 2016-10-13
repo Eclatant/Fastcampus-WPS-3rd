ab20c8858475f64758a6921eaa58797aba5279d3
Lee HanYeong <dev@azelf.com>
2016-10-13 13:13:16 +0900

    WPS auto upload

--- 161013_Django_Day6/mysite-project/django_app/mysite/views.py
+++ 161013_Django_Day6/mysite-project/django_app/mysite/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.http import HttpResponse
 
 
 def test(request, pk1, pk2):
     ret = '%s %s' % (pk1, pk2)
+    return_url = reverse('test', args=(pk1, pk2,))
+    print(return_url)
     return HttpResponse(ret)
