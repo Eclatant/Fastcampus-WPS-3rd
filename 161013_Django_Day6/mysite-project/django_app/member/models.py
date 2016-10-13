aaa895963268a334839efa90447b8ce7fe705cfc
Lee HanYeong <dev@azelf.com>
2016-10-13 17:49:39 +0900

    WPS auto upload

--- 161013_Django_Day6/mysite-project/django_app/member/models.py
+++ 161013_Django_Day6/mysite-project/django_app/member/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import \
     AbstractBaseUser, AbstractUser, \
-    BaseUserManager
+    BaseUserManager, PermissionsMixin
 
 
 class MyUserManager(BaseUserManager):
