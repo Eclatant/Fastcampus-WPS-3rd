6a7e3d55847248a0fa58f4ba585b45cfa3fac4c5
Lee HanYeong <dev@azelf.com>
2016-10-13 16:44:57 +0900

    WPS auto upload

--- 161013_Django_Day6/mysite-project/django_app/member/models.py
+++ 161013_Django_Day6/mysite-project/django_app/member/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(max_length=100, unique=True)
+    last_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=24)
