929f0cd65c1d767c9a0ba8793202300f789a526f
Lee HanYeong <dev@azelf.com>
2016-10-13 16:48:20 +0900

    WPS auto upload

--- 161013_Django_Day6/mysite-project/django_app/member/models.py
+++ 161013_Django_Day6/mysite-project/django_app/member/models.py
@@ -6,6 +6,6 @@ class MyUser(AbstractBaseUser):
     email = models.EmailField(max_length=100, unique=True)
     last_name = models.CharField(max_length=20)
     first_name = models.CharField(max_length=20)
-    nickname = models.CharField(max_length=24)
+    nickname = models.CharField(max_length=24, unique=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
