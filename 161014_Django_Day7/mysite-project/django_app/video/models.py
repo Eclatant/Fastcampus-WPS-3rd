261cdc95e9ab6957a9f3a075884ecc1b33671a13
Lee HanYeong <dev@azelf.com>
2016-10-14 17:01:49 +0900

    WPS auto upload

--- 161014_Django_Day7/mysite-project/django_app/video/models.py
+++ 161014_Django_Day7/mysite-project/django_app/video/models.py
@@ -5,8 +5,9 @@ class Channel(models.Model):
     youtube_id = models.CharField(max_length=100)
     title = models.CharField(max_length=200)
 
+
 class Video(models.Model):
-    
+    youtube_id = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
     published_date = models.DateTimeField()
