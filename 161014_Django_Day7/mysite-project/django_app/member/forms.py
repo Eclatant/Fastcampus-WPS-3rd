075572b79983d23637ba7c801de8816c5d35d334
Lee HanYeong <dev@azelf.com>
2016-10-14 13:37:03 +0900

    WPS auto upload

--- 161014_Django_Day7/mysite-project/django_app/member/forms.py
+++ 161014_Django_Day7/mysite-project/django_app/member/forms.py
@@ -3,9 +3,37 @@ from django import forms
 
 class SignupForm(forms.Form):
     # https://docs.djangoproject.com/en/1.10/topics/forms/modelforms/
-    email = forms.EmailField(max_length=100)
-    password1 = forms.CharField()
-    password2 = forms.CharField()
-    last_name = forms.CharField(max_length=20)
-    first_name = forms.CharField(max_length=20)
-    nickname = forms.CharField(max_length=24)
+    email = forms.EmailField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    last_name = forms.CharField(
+        max_length=20,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    first_name = forms.CharField(
+        max_length=20,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    nickname = forms.CharField(
+        max_length=24,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
+    )
