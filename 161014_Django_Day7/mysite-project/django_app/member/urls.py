1a960ecdb104a18756caefe26584b15fffbd1ca1
Lee HanYeong <dev@azelf.com>
2016-10-14 13:27:12 +0900

    WPS auto upload

--- 161014_Django_Day7/mysite-project/django_app/member/urls.py
+++ 161014_Django_Day7/mysite-project/django_app/member/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', views., name='login'),
+    url(r'^login/$', views.login.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
 ]
