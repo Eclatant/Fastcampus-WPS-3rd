adaca069a9b7e33b45783f845ac9e7c0b09e5aeb
Lee HanYeong <dev@azelf.com>
2016-10-14 13:17:03 +0900

    WPS auto upload

--- 161014_Django_Day7/mysite-project/django_app/member/views.py
+++ 161014_Django_Day7/mysite-project/django_app/member/views.py
@@ -13,28 +13,6 @@ from member.models import MyUser
 # topics/auth/default/#auth-web-requests
 
 
-def login(request):
-    next = request.GET.get('next')
-    if request.method == 'POST':
-        try:
-            username = request.POST['username']
-            password = request.POST['password']
-        except KeyError:
-            return HttpResponse('username 또는 password는 필수항목입니다')
-        user = auth_authenticate(
-            username=username,
-            password=password
-        )
-
-        if user is not None:
-            auth_login(request, user)
-            messages.success(request, '로그인에 성공하였습니다')
-            return redirect(next)
-        else:
-            messages.error(request, '로그인에 실패하였습니다')
-            return render(request, 'member/login.html', {})
-    else:
-        return render(request, 'member/login.html', {})
 
 
 def logout(request):
