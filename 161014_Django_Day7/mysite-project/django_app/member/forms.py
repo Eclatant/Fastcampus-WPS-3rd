796593ea6b2806cb09bc65b43f8d760297d109a7
Lee HanYeong <dev@azelf.com>
2016-10-14 13:11:27 +0900

    WPS auto upload

--- 161014_Django_Day7/mysite-project/django_app/member/forms.py
+++ 161014_Django_Day7/mysite-project/django_app/member/forms.py
@@ -4,4 +4,7 @@ from django import forms
 class SignupForm(forms.Form):
     # https://docs.djangoproject.com/en/1.10/topics/forms/modelforms/
     email = forms.EmailField(max_length=100)
-    # 나머지 요소 작성해보세요
+    last_name = forms.CharField(max_length=20)
+    first_name = forms.CharField(max_length=20)
+    nickname = forms.CharField(max_length=24)
+    date_
