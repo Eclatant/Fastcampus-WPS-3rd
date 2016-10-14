6138da214f1ab3060380c236bbe32c3386092295
Lee HanYeong <dev@azelf.com>
2016-10-14 14:38:13 +0900

    WPS auto upload

--- 161014_Django_Day7/mysite-project/django_app/member/views/signup3.py
+++ 161014_Django_Day7/mysite-project/django_app/member/views/signup3.py
@@ -1,8 +1,10 @@
+from django.shortcuts import render
 from member.forms import SignupModelForm
 
 
 def signup3(request):
+    context = {}
     if request.method == 'POST':
         pass
     else:
-        pass
+        return render(request, 'member/signup2.html', context)
