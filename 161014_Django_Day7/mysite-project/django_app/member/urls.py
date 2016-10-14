06e32f6ecefb5b624f1e41b6ff9781f379c9e483
Lee HanYeong <dev@azelf.com>
2016-10-14 14:38:46 +0900

    WPS auto upload

--- 161014_Django_Day7/mysite-project/django_app/member/urls.py
+++ 161014_Django_Day7/mysite-project/django_app/member/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^signup2/$', views.signup2, name='signup2'),
+    url(r'^signup3/$', views.signup3, name='signup3'),
 ]
--- 161014_Django_Day7/mysite-project/django_app/member/views/__init__.py
+++ 161014_Django_Day7/mysite-project/django_app/member/views/__init__.py
@@ -2,3 +2,4 @@ from .login import login
 from .logout import logout
 from .signup import signup
 from .signup2 import signup2
+from .signup3 import signup3
