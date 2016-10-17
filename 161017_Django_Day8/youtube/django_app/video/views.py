5c3c5f0533754d09d034636b793d7bcf7f43cb14
Lee HanYeong <dev@azelf.com>
2016-10-17 15:24:50 +0900

    WPS auto upload

--- 161017_Django_Day8/youtube/django_app/video/views.py
+++ 161017_Django_Day8/youtube/django_app/video/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from oauth2client.tools import argparser
 
-# Create your views here.
+
+DEVELOPER_KEY = "AIzaSyDiarbwPOxSkXmNPfdv8UtHcZM6KySpk34"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+
+def youtube_search(keyword, max_results=10):
+  youtube = build(
+      YOUTUBE_API_SERVICE_NAME,
+      YOUTUBE_API_VERSION,
+      developerKey=DEVELOPER_KEY
+  )
+
+  search_response = youtube.search().list(
+    q=keyword,
+    part="id,snippet",
+    maxResults=max_results
+  ).execute()
+
+  return search_response
