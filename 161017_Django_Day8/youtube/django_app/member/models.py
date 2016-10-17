6182a6d082d5d74beb4e78d27883aeaee0b189a0
Lee HanYeong <dev@azelf.com>
2016-10-17 13:25:10 +0900

    WPS auto upload

--- 161017_Django_Day8/youtube/django_app/member/models.py
+++ 161017_Django_Day8/youtube/django_app/member/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 
 
+# 기본 장고 유저(auth.User)의 Manager와 User를 상속받아 사용합니다
 class YoutubeUserManager(UserManager):
     pass
 
