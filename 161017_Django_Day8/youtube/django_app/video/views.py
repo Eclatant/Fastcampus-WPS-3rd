84688dc7a01f5c5d48cd749f3991c822904b46d6
Lee HanYeong <dev@azelf.com>
2016-10-17 17:21:44 +0900

    WPS auto upload

--- 161017_Django_Day8/youtube/django_app/video/views.py
+++ 161017_Django_Day8/youtube/django_app/video/views.py
@@ -10,6 +10,14 @@ YOUTUBE_API_VERSION = "v3"
 
 
 def youtube_search(keyword, max_results=10):
+    """
+    youtube_search함수 개선
+
+    1. youtube_search 함수의 arguments에 pageToken 추가
+    2. 받은 pageToken값을 youtube.search()실행 시 list의 인자로 추가
+    3. search뷰에서 request.GET에 pageToken값을 받아오도록 설정
+    4. template에서 이전페이지/다음페이지 a태그 href에 GET parameter가 추가되도록 설정
+    """
     youtube = build(
         YOUTUBE_API_SERVICE_NAME,
         YOUTUBE_API_VERSION,
@@ -19,7 +27,7 @@ def youtube_search(keyword, max_results=10):
     search_response = youtube.search().list(
         q=keyword,
         part="id,snippet",
-        maxResults=max_results
+        maxResults=max_results,
     ).execute()
 
     return search_response
