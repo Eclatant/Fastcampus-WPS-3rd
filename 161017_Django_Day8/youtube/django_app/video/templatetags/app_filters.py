f72c61ff090c491e6709a1e8abc08902bb58f8b2
Lee HanYeong <dev@azelf.com>
2016-10-17 16:39:39 +0900

    WPS auto upload

--- 161017_Django_Day8/youtube/django_app/video/templatetags/app_filters.py
+++ 161017_Django_Day8/youtube/django_app/video/templatetags/app_filters.py
@@ -3,5 +3,6 @@ from django.utils.dateparse import parse_datetime
 register = template.Library()
 
 
+@register.filter(name='string_to_date')
 def string_to_date(value):
     return parse_datetime(value)
