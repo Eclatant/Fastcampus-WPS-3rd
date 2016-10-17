b8f9f81597208995ca4cab436f465808e62f9fbe
Lee HanYeong <dev@azelf.com>
2016-10-17 13:24:37 +0900

    WPS auto upload

--- 161017_Django_Day8/youtube/django_app/member/models.py
+++ 161017_Django_Day8/youtube/django_app/member/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+
+class YoutubeUserManager(UserManager):
+    pass
+
+
+class YoutubeUser(AbstractUser):
+    pass
+
