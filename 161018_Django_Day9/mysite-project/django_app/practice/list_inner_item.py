3a7e5b0b5a2c0b6529fe34b027236776fe3bbc93
Lee HanYeong <dev@azelf.com>
2016-10-18 17:35:55 +0900

    WPS auto upload

--- 161018_Django_Day9/mysite-project/django_app/practice/list_inner_item.py
+++ 161018_Django_Day9/mysite-project/django_app/practice/list_inner_item.py
@@ -1,18 +1,5 @@
 response = {
-    'items': [
-        {
-            'id': 'Item1',
-        },
-        {
-            'id': 'Item1',
-        },
-        {
-            'id': 'Item1',
-        },
-        {
-            'id': 'Item1',
-        },
-    ]
+    'items': [{'id': 'Item1'},{'id': 'Item2'},{'id': 'Item3'},{'id': 'Item4'}]
 }
 id_list = []
 for item in response['items']:
