09267a7a54386fc0d928e1352c12ab257786cb4b
Lee HanYeong <dev@azelf.com>
2016-10-18 17:33:40 +0900

    WPS auto upload

---
+++ 161018_Django_Day9/mysite-project/django_app/practice/list_inner_item.py
@@ -0,0 +1,18 @@
+response = {
+    'items': [
+        {
+            'id': 'Item1',
+        },
+        {
+            'id': 'Item1',
+        },
+        {
+            'id': 'Item1',
+        },
+        {
+            'id': 'Item1',
+        },
+    ]
+}
+
+id_list = []
