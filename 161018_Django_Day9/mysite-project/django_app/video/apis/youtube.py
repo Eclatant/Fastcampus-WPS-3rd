16815a60ec5598022ba865b4774fd8e6bbb72d31
Lee HanYeong <dev@azelf.com>
2016-10-18 17:09:37 +0900

    WPS auto upload

--- 161018_Django_Day9/mysite-project/django_app/video/apis/youtube.py
+++ 161018_Django_Day9/mysite-project/django_app/video/apis/youtube.py
@@ -40,7 +40,11 @@ def youtube_search(keyword, page_token, max_results=10):
         if Video.objects.filter(youtube_id=cur_video_id).exists():
            item['is_exist'] = True
 
-    #
+    # 매 item마다 모든 Video 인스턴스를 검색하지 않고,
+    # 모든 item의 video_id값 리스트(video_id_list)를 이용해서
+    # 현재 검색결과에 해당하는 Video인스턴스 리스트를 미리 계산(exist_list)
+    # 그리고 해당 Video인스턴스 리스트에서 video_id값만 리스트로 빼서 (exist_id_list)
+    # items를 forloop하며 video_id값이 검색결과에 해당하는 exist_id_list에 포함되는지 검사해서 is_exist를 True대입
     video_id_list = [item['id']['videoId'] for item in search_response['items']]
     exist_list = Video.objects.filter(youtube_id__in=video_id_list)
     exist_id_list = [video.youtube_id for video in exist_list]
