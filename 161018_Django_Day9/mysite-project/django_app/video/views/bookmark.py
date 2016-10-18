f5dd31f88ab472b905794f7af0081db367088aa0
Lee HanYeong <dev@azelf.com>
2016-10-18 15:31:56 +0900

    WPS auto upload

--- 161018_Django_Day9/mysite-project/django_app/video/views/bookmark.py
+++ 161018_Django_Day9/mysite-project/django_app/video/views/bookmark.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from video.models import Video
 __all__ = ['add_bookmark', 'bookmark_list']
 
+
 def add_bookmark(request):
     """
     POST요청을 받음
@@ -11,14 +12,20 @@ def add_bookmark(request):
     title
     description
     publishedAt
-    thumbnails
+    thumbnail
 
     요소들을 사용해서​
         Video 인스턴스 생성 후
         받았던 페이지로 돌아가기
         request.path값을 POST안에 받아서 돌아와야 됨
     """
-    pass
+    kind = request.POST['kind']
+    video_id = request.POST['video_id']
+    title = request.POST['title']
+    description = request.POST['description']
+    published_date = request.POST['published_date']
+    thumbnail_url = request.POST['thumbnail_url']
+
 
 
 def bookmark_list(request):
