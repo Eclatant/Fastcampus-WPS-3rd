bab2d3d79f2e22d112d291e979b9821f7a293733
Lee HanYeong <dev@azelf.com>
2016-10-18 15:25:44 +0900

    WPS auto upload

--- 161018_Django_Day9/mysite-project/django_app/video/urls.py
+++ 161018_Django_Day9/mysite-project/django_app/video/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from video.views.bookmark import bookmark_list, add_bookmark
+from video import views
 
 urlpatterns = [
     url(r'^search/$', views.search, name='search'),
