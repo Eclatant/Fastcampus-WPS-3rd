657911b0ff4fda530babae32c4a0b8ec236f04f2
Lee HanYeong <dev@azelf.com>
2016-10-21 15:30:31 +0900

    WPS auto upload

--- 161021_Django_Day11/mysite-project/django_app/sns/views/facebook.py
+++ 161021_Django_Day11/mysite-project/django_app/sns/views/facebook.py
@@ -1,6 +1,23 @@
+import requests
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+from member.apis import facebook
+
 __all__ = [
     'friends_ranking',
 ]
 
+
 def friends_ranking(request):
-    pass
+    if request.GET.get('error'):
+        return HttpResponse('사용자 로그인 거부')
+    if request.GET.get('code'):
+        redirect_url = 'http://{host}{url}'.format(
+            host=request.META['HTTP_HOST'],
+            url=reverse('sns:friends_ranking')
+        )
+        print('redirect_url : %s' % redirect_url)
+        code = request.GET.get('code')
+        access_token = facebook.get_access_token(code, redirect_url)
+        return HttpResponse('%s<br>%s' % (redirect_url, access_token))
