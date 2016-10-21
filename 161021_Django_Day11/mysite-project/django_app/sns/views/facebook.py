f17dd8c387570acdaf2bc78be625434384519ae8
Lee HanYeong <dev@azelf.com>
2016-10-21 15:49:23 +0900

    WPS auto upload

--- 161021_Django_Day11/mysite-project/django_app/sns/views/facebook.py
+++ 161021_Django_Day11/mysite-project/django_app/sns/views/facebook.py
@@ -24,7 +24,7 @@ def friends_ranking(request):
         user_id = facebook.get_user_id_from_token(access_token)
 
         url_request_feed = 'https://graph.facebook.com/v2.8/{user_id}/feed?' \
-                           'fields=comments\{comments\}&' \
+                           'fields=comments{{from,comments}}&' \
                            'access_token={access_token}'.format(
                                 user_id=user_id,
                                 access_token=access_token,
