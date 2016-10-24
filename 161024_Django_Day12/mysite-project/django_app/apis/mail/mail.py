df61e09b795c824a95381b3d7a59f089ec525f1c
Lee HanYeong <dev@azelf.com>
2016-10-24 15:46:18 +0900

    WPS auto upload

--- 161024_Django_Day12/mysite-project/django_app/apis/mail/mail.py
+++ 161024_Django_Day12/mysite-project/django_app/apis/mail/mail.py
@@ -1,13 +1,23 @@
-from django.core.mail import send_mail
+from django.core.mail import send_mail as django_send_mail
 from django.conf import settings
 __all__ = [
     'send_test',
 ]
+
+
 def send_test():
-    send_mail(
+    django_send_mail(
         'Subject',
         'Message',
         settings.EMAIL_HOST_USER,
         ['arcanelux@gmail.com']
     )
 
+def send_mail(subject, message, recipient_list=None):
+    default_recipient_list = ['arcanelux@gmail.com']
+    django_send_mail(
+        subject=subject,
+        message=message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=recipient_list if recipient_list else default_recipient_list
+    )
