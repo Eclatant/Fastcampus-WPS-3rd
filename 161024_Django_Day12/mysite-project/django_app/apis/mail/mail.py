622d849bd424f078f5e90fa72897ad3bec12bfae
Lee HanYeong <dev@azelf.com>
2016-10-24 15:46:52 +0900

    WPS auto upload

--- 161024_Django_Day12/mysite-project/django_app/apis/mail/mail.py
+++ 161024_Django_Day12/mysite-project/django_app/apis/mail/mail.py
@@ -13,11 +13,13 @@ def send_test():
         ['arcanelux@gmail.com']
     )
 
+
 def send_mail(subject, message, recipient_list=None):
     default_recipient_list = ['arcanelux@gmail.com']
     django_send_mail(
         subject=subject,
         message=message,
         from_email=settings.EMAIL_HOST_USER,
+        # if recipient_list가 있을 경우에는 recipient_list, 없으면(None) default_recipient_list를 대입
         recipient_list=recipient_list if recipient_list else default_recipient_list
     )
