f2d76dc8cf40f34c9f56fd83e06d89f55393bafd
Lee HanYeong <dev@azelf.com>
2016-10-24 16:23:24 +0900

    WPS auto upload

--- 161024_Django_Day12/mysite-project/django_app/blog/models.py
+++ 161024_Django_Day12/mysite-project/django_app/blog/models.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from apis.mail import send_mail
 
 
@@ -40,7 +42,7 @@ class Comment(models.Model):
         # send_mail(title, content)
 
 
-@
+@receiver(post_save, sender=Comment)
 def send_comment_mail(sender, instance, **kwargs):
     title = '{} 글에 댓글이 달렸습니다'.format(instance.post.title)
     content = '{}에 {}내용이 달렸네요'.format(
@@ -48,5 +50,5 @@ def send_comment_mail(sender, instance, **kwargs):
         instance.content
     )
     send_mail(title, content)
-
-post_save.connect(send_comment_mail, sender=Comment)
+#
+# post_save.connect(send_comment_mail, sender=Comment)
