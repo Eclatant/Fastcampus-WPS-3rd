74030571537b06944805d738937206cf56699e7b
Lee HanYeong <dev@azelf.com>
2016-10-24 14:31:23 +0900

    WPS auto upload

--- 161024_Django_Day12/mysite-project/django_app/blog/models.py
+++ 161024_Django_Day12/mysite-project/django_app/blog/models.py
@@ -21,5 +21,6 @@ class Post(models.Model):
 
 # 댓글을 저장하는 데이터베이스 모델
 class Comment(models.Model):
+    post = models.ForeignKey(Post)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
