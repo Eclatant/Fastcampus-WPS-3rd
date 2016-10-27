284420230b03cbf574359fd29e7d659514843dc7
Lee HanYeong <dev@azelf.com>
2016-10-27 15:13:20 +0900

    album_add기능 구현

--- 161027_Django_Day14/mysite-project/django_app/photo/views/album.py
+++ 161027_Django_Day14/mysite-project/django_app/photo/views/album.py
@@ -1,9 +1,11 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from ..models import Album
+from ..forms import AlbumForm
 
 __all__ = [
     'album_list',
+    'album_add',
 ]
 
 
@@ -13,3 +15,23 @@ def album_list(request):
         'albums': albums,
     }
     return render(request, 'photo/album_list.html', context)
+
+
+def album_add(request):
+    if request.method == 'POST':
+        form = AlbumForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            description = form.cleaned_data['description']
+            Album.objects.create(
+                title=title,
+                description=description,
+                owner=request.user,
+            )
+            return redirect('photo:album_list')
+    else:
+        form = AlbumForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'photo/album_add.html', context)
