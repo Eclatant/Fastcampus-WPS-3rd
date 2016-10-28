6eddec3df25f52a94d78c80c028d512a1d17ba88
Lee HanYeong <dev@azelf.com>
2016-10-28 16:47:43 +0900

    ajax좋아요/싫어요처리

--- 161028_Django_Day15/mysite-project/django_app/photo/ajax/photo.py
+++ 161028_Django_Day15/mysite-project/django_app/photo/ajax/photo.py
@@ -1,5 +1,5 @@
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
@@ -24,10 +24,12 @@ def photo_like(request, pk, like_type='like'):
         user=request.user,
         photo=photo
     )
+
+    is_delete = False
     # 요청한 유저가 이미 좋아요(또는 싫어요)를 했는가?
     if user_like_exist.exists():
         user_like_exist.delete()
-        msg = 'delete'
+        is_delete = True
     # 이미 누르지 않은 경우, 좋아요 처리를 해준다
     else:
         like_model.objects.create(
@@ -40,8 +42,17 @@ def photo_like(request, pk, like_type='like'):
             user=request.user,
             photo=photo
         ).delete()
-        msg = 'created'
     ret = {
-        'msg': msg
+        'like_count': photo.like_users.count(),
+        'dislike_count': photo.dislike_users.count(),
+        'user_like': False,
+        'user_dislike': False,
+        # 'user_like': True if photo.like_users.filter(pk=request.user.pk).exists() else False,
+        # 'user_dislike': True if photo.dislike_users.filter(pk=request.user.pk).exists() else False,
     }
+    if not is_delete:
+        ret['user_like'] = True if like_type == 'like' else False
+        ret['user_dislike'] = True if like_type != 'like' else False
+
+    # return HttpResponseBadRequest(json.dumps(ret), content_type='application/json')
     return HttpResponse(json.dumps(ret), content_type='application/json')
