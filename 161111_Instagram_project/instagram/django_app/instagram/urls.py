d8671c011afa9f4a1859d36a5f11bec11a8f429d
Lee HanYeong <dev@azelf.com>
2016-11-11 17:19:39 +0900

    WPS auto upload

--- 161111_Instagram_project/instagram/django_app/instagram/urls.py
+++ 161111_Instagram_project/instagram/django_app/instagram/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
+
 
 apis_patterns = [
     url(r'^photo/', include('photo.urls.apis')),
@@ -23,4 +26,4 @@ apis_patterns = [
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(apis_patterns)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
