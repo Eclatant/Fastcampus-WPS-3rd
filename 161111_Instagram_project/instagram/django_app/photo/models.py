cb2423102a925f90f7994e69fadcd53b15a17220
Lee HanYeong <dev@azelf.com>
2016-11-11 14:56:43 +0900

    WPS auto upload

--- 161111_Instagram_project/instagram/django_app/photo/models.py
+++ 161111_Instagram_project/instagram/django_app/photo/models.py
@@ -3,4 +3,22 @@ from django.conf import settings
 
 
 class Photo(models.Model):
+    image = models.ImageField(upload_to='photo', blank=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content = models.TextField(blank=True)
+    # tags = models.
+
+
+class PhotoTag(models.Model):
+    title = models.CharField(max_length=200)
+
+
+class PhotoComment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content = models.TextField()
+
+
+class PhotoLike(models.Model):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
