02b4784083a1fdd196d7605f95fba58a38259086
Lee HanYeong <dev@azelf.com>
2016-11-11 15:37:24 +0900

    WPS auto upload

--- 161111_Instagram_project/instagram/django_app/member/admin.py
+++ 161111_Instagram_project/instagram/django_app/member/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import MyUser
 
-# Register your models here.
+
+class MyUserAdmin(UserAdmin):
+    pass
+
+
+admin.site.register(MyUser, MyUserAdmin)
