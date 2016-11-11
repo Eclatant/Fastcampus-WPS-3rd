ee9abac3157ba25fa0d85c0494664c25fb075e6c
Lee HanYeong <dev@azelf.com>
2016-11-11 14:33:04 +0900

    WPS auto upload

--- 161111_Instagram_project/instagram/django_app/member/models.py
+++ 161111_Instagram_project/instagram/django_app/member/models.py
@@ -34,7 +34,6 @@ class MyUser(AbstractUser):
         return self.following_users.filter(following_users=self)
 
     def follow(self, user):
-        # instance = Following.objects.create(
         instance, created = Following.objects.get_or_create(
             follower=self,
             followee=user
