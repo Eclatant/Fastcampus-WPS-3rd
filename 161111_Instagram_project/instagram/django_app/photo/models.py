87ddd577159f05b62310c3f787e3a1a3bd65e8ab
Lee HanYeong <dev@azelf.com>
2016-11-11 15:02:18 +0900

    WPS auto upload

--- 161111_Instagram_project/instagram/django_app/photo/models.py
+++ 161111_Instagram_project/instagram/django_app/photo/models.py
@@ -8,7 +8,8 @@ class Photo(models.Model):
     content = models.TextField(blank=True)
     tags = models.ManyToManyField('PhotoTag')
     like_users = models.ManyToManyField(
-        settings.AUTH_USER_MODEL,
+        # settings.AUTH_USER_MODEL,
+        # 'member.MyUser',
         through='PhotoLike',
         # related_name='photo_set_like_users'
     )
