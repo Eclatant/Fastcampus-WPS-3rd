dcdd5e0d61f6072d02d6dc77e50681fdec72f618
Lee HanYeong <dev@azelf.com>
2016-11-11 14:02:42 +0900

    WPS auto upload

--- 161111_Instagram_project/instagram/django_app/member/tests.py
+++ 161111_Instagram_project/instagram/django_app/member/tests.py
@@ -28,22 +28,38 @@ class FollowTest(LiveServerTestCase):
         u3.follow(u2)
         u3.follow(u1)
 
-        print(u1.following_users.all())
-        print(u2.following_users.all())
-        print(u3.following_users.all())
-
-        print(u1.follower_users.all())
+        # print(u1.following_users.all())
+        # print(u2.following_users.all())
+        # print(u3.following_users.all())
+        #
+        # print(u1.follower_users.all())
 
     def test_friends(self):
         u1 = self.create_user('u1', '방', '민아')
         u2 = self.create_user('u2', '이', '한영')
         u3 = self.create_user('u3', '박', '성환')
 
+        # 민아는 한영을 팔로우
         u1.follow(u2)
 
+        # 한영은 민아와 성환을 팔로우
         u2.follow(u1)
+        u2.follow(u3)
+
+        # 성환은 한영과 민아를 팔로우
         u3.follow(u2)
         u3.follow(u1)
 
+        # 민아의 친구들 목록
         u1_friends = u1.following_users.filter(following_users=u1)
+
+        # 한영의 친구들 목록
+        u2_friends = u2.following_users.filter(following_users=u2)
+
+        # 성환의 친구들 목록
+        u3_friends = u3.following_users.filter(following_users=u3)
+
+
         print(u1_friends)
+        print(u2_friends)
+        print(u3_friends)
