e6816b491e9c737892038c52a3a461faeba5d622
Lee HanYeong <dev@azelf.com>
2016-11-14 15:35:51 +0900

    WPS auto upload

--- 161114_Instagram_project_cbv/instagram/django_app/photo/urls/views.py
+++ 161114_Instagram_project_cbv/instagram/django_app/photo/urls/views.py
@@ -4,4 +4,5 @@ from .. import views
 urlpatterns = [
     # url(r'^photo/$', views.photo_list, name='photo_list'),
     url(r'^photo/$', views.PhotoList.as_view(), name='photo_list'),
+    url(r'^photo/add/$', views.PhotoAdd.as_view(), name='photo_add'),
 ]
