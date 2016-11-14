5ccb2f9dd549a21ae7b790c883074fc035bfb0e6
Lee HanYeong <dev@azelf.com>
2016-11-14 15:44:08 +0900

    WPS auto upload

--- 161114_Instagram_project_cbv/instagram/django_app/photo/views.py
+++ 161114_Instagram_project_cbv/instagram/django_app/photo/views.py
@@ -22,8 +22,8 @@ class PhotoList(ListView):
 
 class PhotoAdd(CreateView):
     model = Photo
-    fields = ['image', 'author', 'content']
+    fields = ['image', 'content']
 
     def form_valid(self, form):
-        # form.instance.author = self.request.user
+        form.instance.author = self.request.user
         return super(PhotoAdd, self).form_valid(form)
