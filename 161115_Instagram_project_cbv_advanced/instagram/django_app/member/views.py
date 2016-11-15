2fb49de8a4ce81d06518776735a5c2e266055967
Lee HanYeong <dev@azelf.com>
2016-11-15 14:28:15 +0900

    WPS auto upload

--- 161115_Instagram_project_cbv_advanced/instagram/django_app/member/views.py
+++ 161115_Instagram_project_cbv_advanced/instagram/django_app/member/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .forms import LoginForm
 
 
 def login_fbv(request):
     context = {
-        
+        'form': LoginForm(),
     }
     return render(request, 'member/login.html', context)
