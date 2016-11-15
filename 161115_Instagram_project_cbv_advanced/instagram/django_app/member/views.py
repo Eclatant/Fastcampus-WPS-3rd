46e9f3f0bf65aaf2ee89f25063232721f91eeefc
Lee HanYeong <dev@azelf.com>
2016-11-15 15:27:29 +0900

    WPS auto upload

--- 161115_Instagram_project_cbv_advanced/instagram/django_app/member/views.py
+++ 161115_Instagram_project_cbv_advanced/instagram/django_app/member/views.py
@@ -32,6 +32,7 @@ def login_fbv(request):
 class LoginFormView(FormView):
     template_name = 'member/login.html'
     form_class = LoginForm
+    success_url = reverse_lazy('photo:photo_list')
 
     def form_valid(self, form):
         username = form.cleaned_data['username']
@@ -40,7 +41,6 @@ class LoginFormView(FormView):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(self.request, user)
-            # return redirect('photo:photo_list')
         else:
             return HttpResponse('ID or PW incorrect')
         return super().form_valid(form)
