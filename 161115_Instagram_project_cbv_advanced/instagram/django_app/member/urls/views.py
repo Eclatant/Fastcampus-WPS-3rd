ae6807b1d899b1795d04aabbfd245acc4667a438
Lee HanYeong <dev@azelf.com>
2016-11-15 15:05:33 +0900

    WPS auto upload

--- 161115_Instagram_project_cbv_advanced/instagram/django_app/member/urls/views.py
+++ 161115_Instagram_project_cbv_advanced/instagram/django_app/member/urls/views.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .. import views
 
 urlpatterns = [
-    url(r'^login/$', views.login_fbv, name='login'),
+    # url(r'^login/$', views.login_fbv, name='login'),
+    url(r'^login/$', views.LoginFormView.as_view(), name='login'),
 ]
