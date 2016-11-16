4f9ba21237bd9f2217b76f105d53d257fb5de80e
Lee HanYeong <dev@azelf.com>
2016-11-16 16:08:53 +0900

    WPS auto upload

--- 161116_Instagram_project_rest_api/instagram/django_app/photo/apis.py
+++ 161116_Instagram_project_rest_api/instagram/django_app/photo/apis.py
@@ -77,7 +77,7 @@ class PhotoCommentView(APIView):
     def get(self, request, *args, **kwargs):
         print(kwargs)
         if 'photo_pk' in kwargs:
-            photo = get_object_or_404(Photo, **kwargs['photo_pk'])
+            photo = get_object_or_404(Photo, pk=kwargs.get('photo'))
             comments = PhotoComment.objects.filter(photo=photo)
         else:
             comments = PhotoComment.objects.all()
