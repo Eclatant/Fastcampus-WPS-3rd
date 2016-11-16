44ea46cd6dc41f8c8b4ae856b3c5a83632a93120
Lee HanYeong <dev@azelf.com>
2016-11-16 14:43:42 +0900

    WPS auto upload

--- 161116_Instagram_project_rest_api/instagram/django_app/photo/apis.py
+++ 161116_Instagram_project_rest_api/instagram/django_app/photo/apis.py
@@ -3,6 +3,7 @@ import json
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -19,7 +20,11 @@ class PhotoList(APIView):
         return Response(serializer.data)
 
     def post(self, request):
-        
+        serializer = PhotoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def photo_list(request):
