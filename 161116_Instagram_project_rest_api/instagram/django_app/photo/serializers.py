9f64ee94187c8a5b5056c4af86ca7f94b6c806a7
Lee HanYeong <dev@azelf.com>
2016-11-16 16:15:32 +0900

    WPS auto upload

--- 161116_Instagram_project_rest_api/instagram/django_app/photo/serializers.py
+++ 161116_Instagram_project_rest_api/instagram/django_app/photo/serializers.py
@@ -8,6 +8,13 @@ class PhotoSerializer(serializers.ModelSerializer):
         model = Photo
         fields = '__all__'
 
+    def to_representation(self, instance):
+        ret = super().to_representation()
+        ret['commentList'] = PhotoCommentSerializer(
+            data=instance.photocomment_set.all(),
+            many=True).data
+        return ret
+
 
 class PhotoCommentSerializer(serializers.ModelSerializer):
     class Meta:
