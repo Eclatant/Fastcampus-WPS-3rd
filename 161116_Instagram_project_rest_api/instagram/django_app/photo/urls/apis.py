93d7ceaf74695b4500fc79d4151551bd175ef7a0
Lee HanYeong <dev@azelf.com>
2016-11-16 14:32:10 +0900

    WPS auto upload

--- 161116_Instagram_project_rest_api/instagram/django_app/photo/urls/apis.py
+++ 161116_Instagram_project_rest_api/instagram/django_app/photo/urls/apis.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .. import apis
 
 urlpatterns = [
-    url(r'^photo/add/$', apis.photo_add, name='photo_add'),
     url(r'^photo/$', apis.PhotoList.as_view(), name='photo_list'),
+    url(r'^photo/add/$', apis.photo_add, name='photo_add'),
     url(r'^(?P<photo_pk>[0-9]+)/comment/add/$', apis.comment_add, name='comment_add'),
 ]
