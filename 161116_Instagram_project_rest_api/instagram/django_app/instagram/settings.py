c2979b344b932a9347a99cb746caea355fc801f3
Lee HanYeong <dev@azelf.com>
2016-11-16 14:53:43 +0900

    WPS auto upload

--- 161116_Instagram_project_rest_api/instagram/django_app/instagram/settings.py
+++ 161116_Instagram_project_rest_api/instagram/django_app/instagram/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'instagram.middleware.DisableCSRF',
 ]
 
 ROOT_URLCONF = 'instagram.urls'
