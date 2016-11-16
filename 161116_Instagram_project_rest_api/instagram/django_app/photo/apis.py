27716bbcd7eef43a631e22b08a700f3a6ed28089
Lee HanYeong <dev@azelf.com>
2016-11-16 15:33:25 +0900

    WPS auto upload

--- 161116_Instagram_project_rest_api/instagram/django_app/photo/apis.py
+++ 161116_Instagram_project_rest_api/instagram/django_app/photo/apis.py
@@ -63,6 +63,14 @@ def photo_list(request):
     )
 
 
+"""
+Comment API만들어보기
+1. APIView상속받아 get, post에 구현
+2. mixins를 사용해서 list, create에 구현
+3. ViewSet을 사용
+"""
+
+
 @csrf_exempt
 def photo_add(request):
     data = request.POST
