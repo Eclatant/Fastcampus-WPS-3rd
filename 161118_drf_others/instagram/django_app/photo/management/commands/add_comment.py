634d2a9041fbde74ee00bc7f5fcc72ea9c42a78b
Lee HanYeong <dev@azelf.com>
2016-11-18 16:06:11 +0900

    WPS auto upload

--- 161118_drf_others/instagram/django_app/photo/management/commands/add_comment.py
+++ 161118_drf_others/instagram/django_app/photo/management/commands/add_comment.py
@@ -1,7 +1,16 @@
+from django.utils import timezone
 from django.core.management.base import BaseCommand, CommandError
 from photo.models import Photo, PhotoComment
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         photo = Photo.objects.order_by('-created_date').first()
+        content = 'Comment %s' % timezone.now()
+        PhotoComment.objects.create(
+            photo=photo,
+            author=User.objects.first(),
+            content=
+        )
