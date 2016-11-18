c30408dbe3182c4634b2894f0120ae149c6cc4c8
Lee HanYeong <dev@azelf.com>
2016-11-18 13:58:16 +0900

    WPS auto upload

--- 161118_drf_others/instagram/django_app/photo/tasks.py
+++ 161118_drf_others/instagram/django_app/photo/tasks.py
@@ -6,8 +6,10 @@ User = get_user_model()
 
 @shared_task
 def photo_add_after(photo):
+    print('photo_add_after start')
     PhotoComment.objects.create(
         photo=photo,
         author=User.objects.first(),
         content='등록되었습니다',
     )
+    print('photo_add_after end')
