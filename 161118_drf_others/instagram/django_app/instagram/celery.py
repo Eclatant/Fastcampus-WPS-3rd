ec7038bb3337e4ca645ba180b1f9734ee1125b14
Lee HanYeong <dev@azelf.com>
2016-11-18 13:31:45 +0900

    WPS auto upload

--- 161118_drf_others/instagram/django_app/instagram/celery.py
+++ 161118_drf_others/instagram/django_app/instagram/celery.py
@@ -5,3 +5,5 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instagram.settings')
 
 app = Celery('instagram')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
