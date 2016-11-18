3a15650f4fa3e605552d347559a01e04e635b802
Lee HanYeong <dev@azelf.com>
2016-11-18 14:01:41 +0900

    WPS auto upload

--- 161118_drf_others/instagram/django_app/photo/tasks.py
+++ 161118_drf_others/instagram/django_app/photo/tasks.py
@@ -1,3 +1,4 @@
+import time
 from celery import shared_task
 from .models import Photo, PhotoComment
 from django.contrib.auth import get_user_model
@@ -7,6 +8,7 @@ User = get_user_model()
 @shared_task
 def photo_add_after(photo):
     print('photo_add_after start')
+    time.sleep(10)
     PhotoComment.objects.create(
         photo=photo,
         author=User.objects.first(),
